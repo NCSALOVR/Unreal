8df7d3187680380492ab67123edb273978680bb7
Ankoor Shah <amshah4@illinois.edu>
2015-02-03 17:14:43 -0600

    actually fixed this time

--- VIVE_Connector/Source/VIVE_Connector/Private/VIVE_Connector.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
-
-#include "VIVE_Connector.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VIVE_Connector, "VIVE_Connector" );
- 
